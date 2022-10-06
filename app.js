e82cd0577f25449138fbbdc8bad330120a232bbd
Kartiina <katya1gre@gmail.com>
2022-10-06 14:31:18 +0300

    better version

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+// const express = require("express");
+// const {auth} = require("google-auth-library");
+// const {google} = require("googleapis");
+
+// const app = express();
+const express = require("express");
+const { google } = require("googleapis");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    // const auth = new google.auth.GoogleAuth({
+    //     keyFile: "credentials.json",
+    //     scopes: "https://www.googleapis.com/auth/spreadsheets",
+    // });
+    // const client = await auth.getClient();
+
+    // const googleSheets = google.sheets({version: "v4", auth: client});
+
+    // const spreadsheetId = "1deoiyQCCC3KiXlAhraQXcnV6JxoDBz0uslB9Ukn1pu4";
+
+    // const metaData = await googleSheets.spreadsheets.get({
+    //     auth,
+    //     spreadsheetId,
+    // });
+    // const getRows = await googleSheets.spreadsheets.values.get({
+    //     auth,
+    //     spreadsheetId,
+    //     range: "Лист1",
+    // })
+    // metaData.data
+    res.send("hehe");
+});
+
+
+
+
+
+app.listen(1337, (req, res) => console.log("running on 1337"));
+
 const form = document.forms["form"];
 const button = form.elements["button"];
 
